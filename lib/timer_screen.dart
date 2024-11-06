f68bde9293aaaafe0f63b1f6863260448a621496
Dopa91 <code.dopatka@gmail.com>
2024-11-06 17:22:14 +0100

    added new screens, Timer&Stopwatch screen

---
+++ lib/timer_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:projekt_timer_app/stopwatch_function_button.dart';
+
+class TimerScreen extends StatelessWidget {
+  const TimerScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "15 s",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 96,
+            color: Color.fromARGB(255, 14, 65, 107),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            StopwatchFunctionButton(
+              buttonName: 'Start',
+            ),
+            StopwatchFunctionButton(
+              buttonName: 'Stop',
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
