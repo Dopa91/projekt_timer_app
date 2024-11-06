788ca73c7e504eb0951c5bf0703bb25cff12c846
Dopa91 <code.dopatka@gmail.com>
2024-11-06 17:21:12 +0100

    added BottomNavBar to file

---
+++ lib/bottom_navigation_bar_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:projekt_timer_app/stopwatch_screen.dart';
+import 'package:projekt_timer_app/timer_screen.dart';
+
+class BottomNavigationBarScreen extends StatefulWidget {
+  const BottomNavigationBarScreen({
+    super.key,
+  });
+
+  @override
+  State<BottomNavigationBarScreen> createState() =>
+      _BottomNavigationBarMainState();
+}
+
+class _BottomNavigationBarMainState extends State<BottomNavigationBarScreen> {
+  late List<Widget> widgets;
+
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widgets = [
+      const StopwatchScreen(),
+      const TimerScreen(),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        // indicatorShape: const RoundedRectangleBorder(
+        //   borderRadius: BorderRadius.all(
+        //     Radius.circular(8),
+        //   ),
+        // ),
+        selectedIndex: currentIndex,
+        onDestinationSelected: (index) {
+          currentIndex = index;
+          setState(() {});
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.timer),
+            label: "Stopwatch",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.watch_later),
+            label: "Timer",
+          ),
+        ],
+      ),
+      body: widgets[currentIndex],
+    );
+  }
+}
